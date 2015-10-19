a3c2ab452099ce6a4b98a402ee1e6b9b99dc13df
Andres Cisneros <andres.cisneros3@gmail.com>
2015-10-19 22:34:16 +0000

    Final working project, no extras yet

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -17,8 +17,9 @@ class MoviesController < ApplicationController
 
   def create
     @movie = Movie.new(movie_params)
+
       if @movie.save
-        redirect_to_movies_path
+        redirect_to movies_path, notice: "#{@movie.title} was submitted successfully!"
       else 
         render :new
       end
@@ -37,7 +38,7 @@ class MoviesController < ApplicationController
   def destroy
     @movie = Movie.find(params[:id])
     @movie.destroy
-    redirect_to_movie_path 
+    redirect_to movies_path 
   end
 
   protected
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,21 @@
 RottenMangoes::Application.routes.draw do
+  get "reviews/new"
+  get "reviews/create"
+  get "sessions/new"
+  get "sessions/create"
+  # get "users/new"
+  # get "users/create"
+
   # get "movies/index"
   # get "movies/show"
   # get "movies/new"
   # get "movies/edit"
-  resources :movies
+  resources :movies do
+    resources :reviews, only: [:new, :create]
+  end
+  resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
+  root to: 'movies#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
